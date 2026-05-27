e53f727286235a5a5c41800a0447dd020161f619
Neel Gupta <nguptaneelg@gmail.com>
2026-05-27 18:00:36 +0530

    better version

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const atlasConn = mongoose.createConnection(
-  "mongodb+srv://nguptaneelg_db_user:kqmsh6lHbihve2nk@trip.k78upxs.mongodb.net/?appName=trip"
+  
 );
 
 atlasConn.on("connected", () => {
